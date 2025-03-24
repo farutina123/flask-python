0ef4e510ea071f123c7a255216b8e492c7bafb1a
Полина <farutina97@gmail.com>
2025-03-25 01:44:14 +0300

    add html

---
+++ HTML_in_flask/app/__init__.py
@@ -0,0 +1,5 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+from HTML_in_flask.app import routes
---
+++ HTML_in_flask/run.py
@@ -0,0 +1,4 @@
+from HTML_in_flask.app import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
