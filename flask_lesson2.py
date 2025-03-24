c5ad2ca571515f964a06c79c3efc6757a196df38
Полина <farutina97@gmail.com>
2025-03-24 14:54:50 +0300

    second

--- flask_lesson2.py
+++ flask_lesson2.py
@@ -7,17 +7,28 @@ app = Flask(__name__)
 def hello():
     return "Hello, world!"
 
+
 @app.route('/info')
 def info():
     return "This is an informational page."
+
 
 @app.route('/calc/<int:one>/<int:two>')
 def calc(one, two):
     return f"The sum of {one} and {two} is {one+two}."
 
-@app.route('/reverse/<string:reverse_str>')
+
+@app.route('/user/<name>/<int(signed=True):age>')
+def user(name, age):
+    if age < 0:
+        raise ValueError('возраст должен быть положительным')
+    return f'Hello, {name}. You are {age} years old.'
+
+
+@app.route('/reverse/<reverse_str>')
 def rev(reverse_str):
     return reverse_str[::-1]
+
 
 # Запуск приложения
 if __name__ == "__main__":
