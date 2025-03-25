9c78d36a11b6527435538e9cd17f0fc1253cb899
Полина <farutina97@gmail.com>
2025-03-25 17:12:55 +0300

    add html

---
+++ form/app/routes.py
@@ -0,0 +1,23 @@
+from flask import render_template, request, redirect, url_for
+from form.app import app
+
+
+@app.route('/')
+def form():
+    return render_template("form.html")
+
+
+@app.route("/submit", methods=["POST", "GET"])
+def submit():
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        color = request.form.get("color")
+        profession = request.form.get("profession")
+        hobbies = request.form.getlist("hobbies")  # Для чекбоксов
+        level = request.form.get("level")
+        return render_template("result.html", name=name, email=email, color=color, profession=profession,
+                               hobbies=hobbies, level=level)
+    else:
+        return redirect(url_for("form"))
+
