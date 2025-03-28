3e02c6e40d245ea7ebcf0d387021617801b7c7c7
Полина <farutina97@gmail.com>
2025-03-28 18:25:08 +0300

    pr1

--- HTML_in_flask/app/routes.py
+++ HTML_in_flask/app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from HTML_in_flask.app import app
 
 @app.route("/")
@@ -13,3 +13,10 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.route("/submit", methods=["POST", "GET"])
+def submit():
+    if request.method == "POST":
+        return render_template("contact.html", message='Your message has been sent successfully!')
+    else:
+        return redirect(url_for("contact"))
