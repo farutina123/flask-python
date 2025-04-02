23d0dc9b542832b5cdc22497aa180b3b5e172e7e
Полина <farutina97@gmail.com>
2025-04-02 14:55:37 +0300

    jinja2 рабочая версия

--- HTML_in_flask/app/routes.py
+++ HTML_in_flask/app/routes.py
@@ -2,6 +2,11 @@ from flask import render_template, request, redirect, url_for
 from HTML_in_flask.app import app
 from datetime import datetime
 
+def date(value, format="%H:%M %d-%m-%y"):
+    return value.strftime(format)
+
+app.jinja_env.filters["date"] = date
+
 @app.route("/")
 def home():
     current_date = datetime.now()
@@ -9,12 +14,26 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    team_members = [
+        {'name': 'Alice', 'role': 'Developer'},
+        {'name': 'Bob', 'role': 'Designer'},
+        {'name': 'Charlie', 'role': 'Project Manager'}
+    ]
+    return render_template("about.html", team_members=team_members)
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    manager = {
+        'name': 'Alice',
+        'phone': '8-920-497-91-66',
+        'address': {
+            'street': '123 Main St',
+            'city': 'Wonderland',
+            'index': '249405'
+        }
+    }
+    return render_template("contact.html", manager=manager)
 
 @app.route("/submit", methods=["POST", "GET"])
 def submit():
